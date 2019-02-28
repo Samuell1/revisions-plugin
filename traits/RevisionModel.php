69089f1100c8336e56aa751fa9841cdce1c9a5f2
Samuell1 <samuell.patro@gmail.com>
2019-02-28 10:34:10 +0100

    Extend trait to add morphMany relation for revision_history

--- traits/RevisionModel.php
+++ traits/RevisionModel.php
@@ -1,19 +1,30 @@
 <?php namespace Samuell\Revisions\Traits;
 
 use BackendAuth;
+use Exception;
 
 /**
  * Extend model with revision methods
  */
 trait RevisionModel
 {
+    public static function bootRevisionable()
+    {
+        if (!property_exists(get_called_class(), 'revisionable')) {
+            throw new Exception(sprintf(
+                'You must define a $revisionable property in %s to use the Revisionable trait.', get_called_class()
+            ));
+        }
+
+        static::extend(function($model) {
+            $model->morphMany['revision_history'] = ['System\Models\Revision', 'name' => 'revisionable'];
+        });
+    }
+
     public function getRevisionableUser()
     {
         return BackendAuth::getUser()
             ? BackendAuth::getUser()->id
             : null;
     }
-    // public $morphMany = [
-    //     'revision_history' => ['System\Models\Revision', 'name' => 'revisionable']
-    // ];
 }
