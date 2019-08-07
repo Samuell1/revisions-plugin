cf5729b9ed892d5863b25bbc02ed4049c2ffa45a
Samuell <Samuell1@users.noreply.github.com>
2019-08-07 21:18:57 +0200

    Refactoring

--- formwidgets/RevisionHistory.php
+++ formwidgets/RevisionHistory.php
@@ -1,84 +1,59 @@
-<?php namespace Samuell\Revisions\FormWidgets;
+<?php
+
+namespace Samuell\Revisions\FormWidgets;
 
 use Backend\Classes\FormField;
 use Backend\Classes\FormWidgetBase;
 use System\Models\Revision;
 
 use Flash;
-use Input;
 
 /**
  * RevisionHistory Form Widget
  */
 class RevisionHistory extends FormWidgetBase
 {
-    /**
-     * {@inheritDoc}
-     */
     protected $defaultAlias = 'abcreate_eshop_revision_history';
 
-    /**
-     * {@inheritDoc}
-     */
-    public function init()
-    {
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     public function render()
     {
         $this->prepareVars();
         return $this->makePartial('revisionhistory');
     }
 
-    /**
-     * Prepares the form widget view data
-     */
     public function prepareVars()
     {
-        $this->vars['history'] = $this->model->revision_history;
+        $this->vars['history'] = $this->model->revision_history->reverse();
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public function loadAssets()
     {
         $this->addCss('css/revisionhistory.css', 'samuell.revisionhistory');
         $this->addJs('js/revisionhistory.js', 'samuell.revisionhistory');
     }
 
-    /**
-     * {@inheritDoc}
-     */
-     public function getSaveValue($value)
-     {
-         return FormField::NO_SAVE_DATA;
-     }
+    public function getSaveValue($value)
+    {
+        return FormField::NO_SAVE_DATA;
+    }
 
-
-     // Revert ajax handler
-     public function onRevertHistory()
-     {
-        // dynamic load model class
+    public function onRevertHistory()
+    {
         $modelClass = $this->getClass();
         $section = $modelClass::find($this->model->id);
 
-        $revision = Revision::find(Input::get('revision_id'));
+        $revision = Revision::find(input('revision_id'));
 
         $section->{$revision->field} = $revision->old_value;
         $section->save();
 
-        Flash::success('Changes have been restored, changes are not visible without restoring the page.');
+        Flash::success('Changes have been restored, changes are not visible without refreshing the page.');
 
         // TODO REFRESH PAGE
-     }
+    }
 
-     private function getClass()
-     {
-         return get_class($this->model);
-     }
+    private function getClass()
+    {
+        return get_class($this->model);
+    }
 }
