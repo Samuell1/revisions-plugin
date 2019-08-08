b4ab939ccd50648477eb15c93c252620ad9baead
Samuell1 <samuell.patro@gmail.com>
2019-08-08 09:15:29 +0200

    Added better diff

---
+++ classes/Diff.php
@@ -0,0 +1,46 @@
+<?php namespace Samuell\Revisions\Classes;
+
+class Diff
+{
+    private static function diff($old, $new)
+    {
+        $matrix = array();
+        $maxlen = 0;
+        foreach ($old as $oindex => $ovalue) {
+            $nkeys = array_keys($new, $ovalue);
+            foreach ($nkeys as $nindex) {
+                $matrix[$oindex][$nindex] = isset($matrix[$oindex - 1][$nindex - 1]) ?
+                    $matrix[$oindex - 1][$nindex - 1] + 1 : 1;
+                if ($matrix[$oindex][$nindex] > $maxlen) {
+                    $maxlen = $matrix[$oindex][$nindex];
+                    $omax = $oindex + 1 - $maxlen;
+                    $nmax = $nindex + 1 - $maxlen;
+                }
+            }
+        }
+
+        if ($maxlen == 0) {
+            return array(array('d' => $old, 'i' => $new));
+        }
+
+        return array_merge(
+            self::diff(array_slice($old, 0, $omax), array_slice($new, 0, $nmax)),
+            array_slice($new, $nmax, $maxlen),
+            self::diff(array_slice($old, $omax + $maxlen), array_slice($new, $nmax + $maxlen))
+        );
+    }
+
+    public static function htmlDiff($old, $new)
+    {
+        $ret = '';
+        $diff = self::diff(preg_split("/[\s]+/", $old), preg_split("/[\s]+/", $new));
+        foreach ($diff as $k) {
+            if (is_array($k)) {
+                $ret .= (!empty($k['d']) ? "<del>" . implode(' ', $k['d']) . "</del> " : '') . (!empty($k['i']) ? "<ins>" . implode(' ', $k['i']) . "</ins> " : '');
+            } else {
+                $ret .= $k . ' ';
+            }
+        }
+        return $ret;
+    }
+}
