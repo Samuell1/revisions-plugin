54f74614501de54b665ad34694ab2f79b2694938
Ruben van Erk <ruben@rubenvanerk.nl>
2020-08-11 11:31:10 +0200

    Add localization strings and Dutch translation

---
+++ lang/nl/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'revision' => [
+        'system_user' => 'Systeem',
+        'restore' => 'Wijzigingen terugdraaien',
+        'restore_selected' => 'Draai geselecteerde terug',
+        'restore_all' => 'Draai alle terug',
+        'changes_restored' => 'De wijzigingen zijn teruggedraaid, wijzigingen zijn niet zichtbaar zonder de pagina te verversen.',
+    ],
+];
