9aabecf91b46af6b1a71c0b413e67646f7652d06
Eugen <eb@rend.de>
2020-10-12 17:44:22 +0200

    Add new translations

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -9,5 +9,15 @@ return [
         'restore_all' => 'Restore all changes',
         'changes_restored' => 'Changes have been restored, changes are not visible without refreshing the page.',
         'read_only_error' => 'Revisions are set to read only and can not be restored!',
+        'delete_revision' => 'Delete this revision',
+        'delete_all_history' => 'Delete all history',
+        'are_you_sure' => 'Are you sure to delete all history of this model? This cannot be reversed!',
+        'yes_delete' => 'Yes, delete all!',
+    ],
+    'messages' => [
+        'successfully_deleted'     => 'This changes successfully deleted!',
+        'all_successfully_deleted' => 'All changes successfully deleted!',
+        'revision_not_found'       => 'This revision could not be found!',
+        'model_not_found'          => 'Revisions for this model could not be found!',
     ],
 ];
--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -9,5 +9,15 @@ return [
         'restore_all' => 'Draai alle terug',
         'changes_restored' => 'De wijzigingen zijn teruggedraaid, wijzigingen zijn niet zichtbaar zonder de pagina te verversen.',
         'read_only_error' => 'Wijzigingen kunnen alleen gelezen en niet teruggedraaid worden!',
+        'delete_revision' => 'Verwijder deze revisie',
+        'delete_all_history' => 'Wis alle geschiedenis',
+        'are_you_sure' => 'Weet u zeker dat u alle geschiedenis van dit model wilt verwijderen? Dit kan niet worden teruggedraaid!',
+        'yes_delete' => 'Ja, alles verwijderen!',
+    ],
+    'messages' => [
+        'successfully_deleted'     => 'Deze wijzigingen zijn succesvol verwijderd!',
+        'all_successfully_deleted' => 'Alle wijzigingen zijn verwijderd!',
+        'revision_not_found'       => 'Deze revisie is niet gevonden!',
+        'model_not_found'          => 'Revisies voor dit model zijn niet gevonden!',
     ],
 ];
